c5365ced260aa1fa6be387d60f5c4d5b91492c29
aloulou1238 <benhibaali@gmail.com>
2023-05-31 19:54:22 +0200

    Added MongoDB connection

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
+var morgan = require('morgan')
+const Person = require('./models/person')
 
 app.use(express.json())
-const cors = require('cors')
-
 app.use(cors())
 app.use(express.static('build'))
-
-var morgan = require('morgan')
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.use(morgan('tiny'))
 
@@ -45,7 +22,7 @@ const requestLogger = (request, response, next) => {
 app.use(requestLogger)
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => response.json(persons))
 })
 
 app.get('/api/persons/:id', (request, response) => {
